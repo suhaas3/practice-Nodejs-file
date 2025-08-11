8d5dfd99a2ab40accfc9a59559af38e3966c215c
suhaas3 <suhaas3w@gmail.com>
2025-08-11 11:15:31 +0530

    require and module.exports working practice

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+
+const sumOfTwoNumber = require('./sum');
 console.log('this is app.js fiel')
+
+const a=10;
+const b=20;
+sumOfTwoNumber(a,b);
---
+++ sum.js
@@ -0,0 +1,10 @@
+const a=1;
+const b=2;
+
+function sumOfTwoNumber(a,b) {
+  return console.log("sum of two nubers",a+b);
+}
+
+sumOfTwoNumber(a,b);
+
+module.exports = sumOfTwoNumber;

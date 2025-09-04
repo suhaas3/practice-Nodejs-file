74e55bf2bd92e34b4e2f04c98b798d8329e1492c
suhaas3 <suhaas3w@gmail.com>
2025-09-04 09:47:44 +0530

    installing express and create the server

---
+++ AsynchronousCodePractice/my-express-app/server.js
@@ -0,0 +1,20 @@
+const express = require('express');//import the expree module
+
+const app = express();//create an express application instance
+
+const PORT = 3333;//define the port for the project
+
+//define a basic route for the URL
+app.get("/hello",(req,res) => {
+  res.send("hello,node js!");
+})
+
+app.get("/",(req,res) => {
+  res.send("this is the initial data...");
+})
+
+//start the server and listen for incoming requests
+app.listen(PORT,() => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Access it at http://localhost:${PORT}`)
+})

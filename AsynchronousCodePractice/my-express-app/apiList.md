f0b6551d8b90f2ec3a3f366c9c904adbb661ad78
suhaas3 <suhaas3w@gmail.com>
2025-09-18 08:12:01 +0530

    user accepted requests router is implemented

--- AsynchronousCodePractice/my-express-app/apiList.md
+++ AsynchronousCodePractice/my-express-app/apiList.md
@@ -17,8 +17,8 @@
 - POST /request/rewiev/rejected/:requestId
 
 ## userRouter
+- GET /user/requests/received
 - GET /user/connections
-- GET /user/requests
 - GET /user/feed - Gets you the profiles of other users on platform
 
 Status: ignore, interested, accepted, rejected
--- AsynchronousCodePractice/my-express-app/src/routes/user.js
+++ AsynchronousCodePractice/my-express-app/src/routes/user.js
@@ -3,22 +3,53 @@ const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connection');
 const userRouter = express.Router();
 
+  const allow_user_details = "firstName lastName age gender skills about photoUrl";
+
 //Get all the pending connection request for the loggedIn user
 userRouter.get('/user/requests/received', userAuth, async (req, res) => {
+
   try {
     const loggedInUser = req.user;
 
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", ["firstName", "lastName"])
-    
+    }).populate("fromUserId", allow_user_details);
+    // .populate("fromUserId", ["firstName", "lastName"])
+
     res.json({
       message: "Data fetched successfully...",
       data: connectionRequests
     })
   } catch (err) {
-    res.status(400).send("ERROR: "+err.message);
+    res.status(400).send("ERROR: " + err.message);
+  }
+})
+
+userRouter.get('/user/connections', userAuth, async (req, res) => {
+  try {
+
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" }
+      ]
+    }).populate("fromUserId", allow_user_details)
+
+
+    const data = connectionRequests.map(row => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId
+      }
+      return row.fromUserId
+    }) 
+
+    res.json({data})
+
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 })
 
